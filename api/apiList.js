964ae8404906720a49e5e7844eac501a4e7e2cf9
Sumit Nirmal <sumitramprakashnirmal@gmail.com>
2024-09-26 11:54:03 +0530

    /api changes

--- api/apiList.js
+++ api/apiList.js
@@ -3,14 +3,14 @@ export const server = 'https://cograd-erp-backend.onrender.com';
 
 
 const apiList = {
-  login: `${server}/parent/login`,
-  getStudentInfo: studentId => `${server}/student/${studentId}`,
-  fees: parentId => `${server}/parent/feesDetails/${parentId}`,
-  attendance: studentId => `${server}/student/studentAttendance/${studentId}`,
-  progressReport: studentId => `${server}/examResult/${studentId}`,
-  sendComplaint: `${server}/complains/register`,
-  getComplaints: parentId => `${server}/complains/parentComplain/${parentId}`,
-  getNotifications: parentId => `${server}/notifications/parent/${parentId}`,
+  login: `${server}/api/parent/login`,
+  getStudentInfo: studentId => `${server}/api/student/${studentId}`,
+  fees: parentId => `${server}/api/parent/feesDetails/${parentId}`,
+  attendance: studentId => `${server}/api/student/studentAttendance/${studentId}`,
+  progressReport: studentId => `${server}/api/examResult/${studentId}`,
+  sendComplaint: `${server}/api/complains/register`,
+  getComplaints: parentId => `${server}/api/complains/parentComplain/${parentId}`,
+  getNotifications: parentId => `${server}/api/notifications/parent/${parentId}`,
 };
 
 export default apiList;
